152e869bd2433cb70997815d451ab8ab90f11102
Philip Shishov <philshishov@gmail.com>
2021-05-25 22:55:39 +0200

    not-found.component and account module

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
