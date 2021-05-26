726524bcfce6e14698d02981ceffe524aae8c82a
Philip Shishov <philshishov@gmail.com>
2021-05-26 20:20:19 +0200

    Create all components for account module

--- src/client/src/app/modules/account/account.module.ts
+++ src/client/src/app/modules/account/account.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AccountRoutingModule } from './account-routing.module';
 
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { AccessDeniedComponent } from './access-denied/access-denied.component';
+import { ConfirmEmailComponent } from './confirm-email/confirm-email.component';
+import { ForgotPasswordConfirmationComponent } from './forgot-password-confirmation/forgot-password-confirmation.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    LogoutComponent,
+    ProfileComponent,
+    ForgotPasswordComponent,
+    AccessDeniedComponent,
+    ConfirmEmailComponent,
+    ForgotPasswordConfirmationComponent,
+    ResetPasswordComponent
+  ],
   imports: [
     CommonModule,
     AccountRoutingModule
---
+++ src/client/src/app/modules/account/forgot-password/forgot-password.component.ts
@@ -0,0 +1,19 @@
+import { NgForm } from '@angular/forms';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot-password',
+  templateUrl: './forgot-password.component.html',
+  styleUrls: ['./forgot-password.component.scss']
+})
+export class ForgotPasswordComponent {
+
+  constructor() { }
+  email: string;
+
+  onSubmit(ngForm :NgForm){
+    if(ngForm.valid){
+
+    }
+  }
+}
