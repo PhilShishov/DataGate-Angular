2350c085ba3d91340bae6e69ec6834800d383b2d
Philip Shishov <philshishov@gmail.com>
2021-05-26 20:22:59 +0200

    Routing path for account module

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { BrowserModule, Title } from '@angular/platform-browser';
 
-const routes: Routes = [];
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: ''
+  },
+  { path: '', data: { title: 'Funds Management Software' }, loadChildren: () => import('./modules/account/account.module').then(m => m.AccountModule) },
+  {
+    path: '**', component: NotFoundComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
