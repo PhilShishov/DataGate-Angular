38039f7d9da3ffbc00d26f1859b5be84229ae655
Philip Shishov <philshishov@gmail.com>
2021-05-26 20:46:49 +0200

    Refactor IdentityController

--- src/server/Web/DataGate.Web/Api/IdentityController.cs
+++ src/server/Web/DataGate.Web/Api/IdentityController.cs
@@ -1,28 +1,25 @@
-﻿using DataGate.Data.Models.Dtos;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace DataGate.Web.Api
+﻿namespace DataGate.Web.Api
 {
+    using DataGate.Data.Models.Dtos;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    using System.Collections.Generic;
+
     [Route("api/[controller]")]
     [ApiController]
     public class IdentityController : ControllerBase
     {
         [AllowAnonymous]
         [HttpPost("login")]
-        public ActionResult<List<string>> Login([FromBody]UserLoginDto userLoginDto)
+        public ActionResult<List<string>> Login([FromBody] UserLoginDto userLoginDto)
         {
             var countries = new List<string>();
-            countries.Add("India");
+            countries.Add("Italy");
+            countries.Add("Luxembourg");
             countries.Add("Bulgaria");
             return Ok(countries);
         }
-
-
     }
 }
