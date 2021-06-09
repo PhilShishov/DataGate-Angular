de62eefb93849aed297566aef1353099b72991d2
Philip Shishov <philshishov@gmail.com>
2021-06-09 17:02:14 +0200

    Notification and Userpanel controller migrated from razor to json response

---
+++ src/server/Web/DataGate.Web/Api/NotificationController.cs
@@ -0,0 +1,54 @@
+﻿namespace DataGate.Web.Api
+{
+    using DataGate.Services.Notifications.Contracts;
+    using DataGate.Web.Hubs.Contracts;
+    using DataGate.Web.ViewModels.Notifications;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly IHubNotificationHelper notificationHelper;
+        private readonly INotificationService notificationService;
+
+        public NotificationController(
+            IHubNotificationHelper notificationHelper,
+            INotificationService notificationService)
+        {
+            this.notificationHelper = notificationHelper;
+            this.notificationService = notificationService;
+        }
+
+        [HttpGet("all")]
+        public async Task<ActionResult<List<NotificationViewModel>>> All()
+        {
+            var model = await this.notificationService.All<NotificationViewModel>(this.User);
+            int count = await this.notificationService.Count(this.User);
+            await this.notificationHelper.HubToAll(count);
+
+            return Ok(model);
+        }
+
+        [HttpGet("status/{notifId}")]
+        public async Task<ActionResult<NotificationResponseModel>> Status(string notifId)
+        {
+            await this.notificationService.StatusAsync(this.User, notifId);
+            var status = this.notificationService.GetStatus(this.User, notifId);
+
+            return new NotificationResponseModel { Status = status, NotifId = notifId };
+        }
+
+        [HttpGet("statusAll")]
+        public async Task<ActionResult<NotificationResponseModel>> StatusAll()
+        {
+            await this.notificationService.StatusAllAsync(this.User);
+
+            return new NotificationResponseModel();
+        }
+    }
+}
---
+++ src/server/Web/DataGate.Web/Api/UserController.cs
@@ -0,0 +1,39 @@
+﻿using DataGate.Services.Data.Recent;
+using DataGate.Services.Data.ShareClasses;
+using DataGate.Web.ViewModels.Users;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataGate.Web.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController: ControllerBase
+    {
+        private readonly IRecentService recentService;
+        private readonly IShareClassService service;
+
+        public UserController(
+            IRecentService recentService,
+            IShareClassService service)
+        {
+            this.recentService = recentService;
+            this.service = service;
+        }
+
+        [HttpGet("getUserPanelData")]
+        public ActionResult<UserPanelViewModel> GetUserPanelData()
+        {
+            var viewModel = new UserPanelViewModel
+            {
+                ShareClasses = this.service.ByDate(),
+                RecentlyViewed = this.recentService.ByUserId(this.User),
+            };
+
+            return Ok(viewModel);
+        }
+    }
+}
