1a359169ea96ac6ca4070ad3375ea1a2ab3206ee
Philip Shishov <philshishov@gmail.com>
2021-06-26 20:20:10 +0200

    keep global constants.ts

---
+++ src/client/src/app/shared/utils/constants.ts
@@ -0,0 +1,20 @@
+export class DataGateConstants{
+  static userKey = 'user';
+  static usernameKey = 'username';
+  static googleRecaptchaToken = 'recaptchaToken';
+  static googleRecaptchaKey = 'recaptchaKey';
+  static consentKey = 'ConsentCookie'
+  static indexEntityIdInTable = 0;
+  static FundsController = "Funds";
+  static FundArea = "Fund";
+  static ActionDetails = "Details";
+  static DisplaySub = "Sub";
+  static ShareClassesController = "ShareClasses";
+  static ShareClassArea = "ShareClass";
+  static SubFundShareClassesController = DataGateConstants.DisplaySub + DataGateConstants.FundArea + DataGateConstants.ShareClassesController;
+  static FundAbbreviation = "f";
+  static SubFundAbbreviation =  "sf";
+  static ShareClassAbbreviation = "sc";
+  static RouteDetails = "details";
+  static AppFooter = "  DataGate - All rights reserved";
+}
