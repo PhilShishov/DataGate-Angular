1bd10a6a9ca2a2f278c2ed3118805c922f5f6942
Philip Shishov <philshishov@gmail.com>
2021-07-03 21:21:01 +0200

    userpanel component with user and admin role

--- src/client/src/app/modules/user-panel/container/user-panel.component.ts
+++ src/client/src/app/modules/user-panel/container/user-panel.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CoreCacheService } from 'src/app/core/cache/core-cache.service';
+import { PageNotFoundHandler } from 'src/app/core/errorHandler/pageNotFoundHandler';
+import { IUserPanelViewModel } from 'src/app/shared/interfaces/IUserPanelViewModel';
+import { DataGateConstants } from 'src/app/shared/utils/constants';
+import { UserPanelService } from '../user-panel.service';
 
 @Component({
   selector: 'app-user-panel',
@@ -6,10 +11,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-panel.component.scss']
 })
 export class UserPanelComponent implements OnInit {
+  userPanelData: IUserPanelViewModel = {} as IUserPanelViewModel;
+  hasAdminRole: boolean;
+  shownotFoundPage: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private userPanelService: UserPanelService,
+    private coreCacheService: CoreCacheService,
+    private pageNotFoundHandler: PageNotFoundHandler
+  ) {
+    this.pageNotFoundHandler.listenNotFoundAuth().subscribe(data => {
+      this.shownotFoundPage = data.notFound;
+    });
   }
 
+  ngOnInit(): void {
+    this.userPanelService.getUserPanelData().subscribe(res => {
+      this.userPanelData = res;
+      let user = this.coreCacheService.getByKey(DataGateConstants.userKey);
+      this.hasAdminRole = user.roles != null && user.roles > 0 && user.roles.indexOf('Admin') > -1;
+    });
+  }
 }
