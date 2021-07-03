32a49eb25b43838450c85e8d423bf7202dd7e843
Philip Shishov <philshishov@gmail.com>
2021-07-03 21:33:50 +0200

    update app.component with auth and non-auth home page

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { ActivatedRoute, Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { CoreCacheService } from './core/cache/core-cache.service';
+import { PageNotFoundHandler } from './core/errorHandler/pageNotFoundHandler';
+import { NotFoundInfo } from './shared/interfaces/notFound';
+import { DataGateConstants } from './shared/utils/constants';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +15,13 @@ import { TranslateService } from '@ngx-translate/core';
 export class AppComponent {
   title = 'DataGate';
   titleKey = "Title";
+  pageNotFoundTitle: 'Page Not Found';
 
   constructor(private titleService: Title,
     private translateService: TranslateService,
-    private router: Router,
-    private activatedRoute: ActivatedRoute
+    private readonly router: Router,
+    private coreCacheService: CoreCacheService,
+    private pageNotFoundHandler: PageNotFoundHandler
   ) {
     this.translateService.addLangs(['en', 'it']);
     translateService.setDefaultLang('en');
@@ -24,5 +30,23 @@ export class AppComponent {
 
   ngOnInit() {
     this.titleService.setTitle(this.title);
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        if (event.url != event.urlAfterRedirects) {
+          let user = this.coreCacheService.getByKey(DataGateConstants.userKey);
+          let data = { authenticated: false, notFound: true } as NotFoundInfo
+          if (user) {
+            data.authenticated = true;
+            this.router.navigate(['/userpanel']);
+            this.pageNotFoundHandler.fillNotFoundAuth(data);
+          } else {
+            data.authenticated = false;
+            this.router.navigate(['/']);
+            this.pageNotFoundHandler.fillNotFoundNoAuth(data);
+          }
+          this.titleService.setTitle(this.pageNotFoundTitle);
+        }
+      }
+    })
   }
 }
