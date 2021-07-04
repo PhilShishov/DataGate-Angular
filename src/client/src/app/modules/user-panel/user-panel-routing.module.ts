a241896d84e6d5e20ffcdb543c580e0f99c17f08
Philip Shishov <philshishov@gmail.com>
2021-07-04 19:38:16 +0200

    fix routing for userpanel to match home page and add all components to module

--- src/client/src/app/modules/user-panel/user-panel-routing.module.ts
+++ src/client/src/app/modules/user-panel/user-panel-routing.module.ts
@@ -1,16 +1,16 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from "@angular/core";
 
-import { UserPanelComponent } from './container/user-panel.component';
 import { AppLayoutComponent } from '../../core/layouts/app-layout/components/container/app-layout.component';
 
 import { AuthGuardService } from '../../core/guards/auth-guard.service';
+import { UserPanelComponent } from './container/user-panel.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AppLayoutComponent,
-    // canActivate: [AuthGuardService],
+    canActivate: [AuthGuardService],
     children: [
       { path: '', component: UserPanelComponent }
     ]
--- src/client/src/app/modules/user-panel/user-panel.module.ts
+++ src/client/src/app/modules/user-panel/user-panel.module.ts
@@ -1,17 +1,31 @@
+import { WebDatePipe } from './../../shared/pipes/webDate.pipe';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 import { NgModule } from "@angular/core";
 
 import { AppLayoutModule } from "src/app/core/layouts/app-layout/app-layout.module";
 import { UserPanelRoutingModule } from "./user-panel-routing.module";
 
+import { SliderComponent } from './components/slider/slider.component';
+import { UserPanelService } from './user-panel.service';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { UserPanelComponent } from './container/user-panel.component';
 
 @NgModule({
   imports: [
     AppLayoutModule,
-    UserPanelRoutingModule
+    UserPanelRoutingModule,
+    TranslateModule,
+    CommonModule,
+    SlickCarouselModule
   ],
   declarations: [
-    UserPanelComponent
-  ]
+    UserPanelComponent,
+    SliderComponent,
+    NotFoundComponent,
+    WebDatePipe
+  ],
+  providers: [UserPanelService]
 })
 export class UserPanelModule{}
