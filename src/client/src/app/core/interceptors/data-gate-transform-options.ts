81331a28d57baf70ac42e864742894da9e73a6a6
Philip Shishov <philshishov@gmail.com>
2021-08-10 22:25:38 +0300

    replace snackbar with Toastr and complete the observable interceptor

--- src/client/src/app/core/interceptors/data-gate-transform-options.ts
+++ src/client/src/app/core/interceptors/data-gate-transform-options.ts
@@ -1,52 +1,99 @@
-import { SnackBarsService } from './../layouts/ui-input/snackbars/services/snackbars.service';
-import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+  HttpResponse,
+} from '@angular/common/http';
 import { Injector, Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
 import { LoaderService } from '../../shared/utils/loader.service';
 import { TokenService } from '../../shared/utils/token.service';
+import { AccountService } from 'src/app/modules/account/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class DataGateTransformOptions implements HttpInterceptor {
-
-  loaderService: LoaderService
-  tokenService: TokenService
-  snackBarsService: SnackBarsService;
+  loaderService: LoaderService;
+  tokenService: TokenService;
+  toaster: ToastrService;
+  accountService: AccountService;
 
   constructor(private _injector: Injector) {
     this.loaderService = this._injector.get(LoaderService);
     this.tokenService = this._injector.get(TokenService);
-    this.snackBarsService = this._injector.get(SnackBarsService);
+    this.toaster = this._injector.get(ToastrService);
+    this.accountService = this._injector.get(AccountService);
   }
 
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    this.loaderService.activateLoader();
+  private requests: HttpRequest<any>[] = [];
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    this.loaderService.isLoading.next(true);
     const token: string = this.tokenService.getToken();
-    if (token) {
-      request = request.clone({ headers: request.headers.set('Authorization', 'Bearer ' + token) });
-    }
-    request = request.clone({ headers: request.headers.set('Content-Type', 'application/json') });
-    request = request.clone({ headers: request.headers.set('Accept', 'application/json') });
-    request = request.clone({
-      withCredentials: true
-    });
-    if(request.method.toUpperCase() == 'POST'){
+    if (this.accountService.currentUser)
       request = request.clone({
-        body: JSON.stringify(request.body)
-      })
+        headers: request.headers.set(
+          'userId',
+          this.accountService.currentUser.id
+        ),
+      });
+    if (token) {
+      request = request.clone({
+        headers: request.headers.set('Authorization', 'Bearer ' + token),
+      });
     }
-    return next.handle(request).pipe(
-      map((event: HttpEvent<any>) => {
-        if (event instanceof HttpResponse) {
-          this.loaderService.deactivateLoader();
+    request = request.clone({
+      headers: request.headers.set('Content-Type', 'application/json'),
+    });
+    request = request.clone({
+      headers: request.headers.set('Accept', 'application/json'),
+    });
+    request = request.clone({
+      withCredentials: true,
+    });
+    if (request.method.toUpperCase() == 'POST') {
+      request = request.clone({
+        body: JSON.stringify(request.body),
+      });
+    }
+
+    return Observable.create((observer) => {
+      const subscription = next.handle(request).subscribe(
+        (event) => {
+          if (event instanceof HttpResponse) {
+            this.removeRequest(request);
+            observer.next(event);
+          }
+        },
+        (err) => {
+          this.removeRequest(request);
+          observer.error(err);
+        },
+        () => {
+          this.removeRequest(request);
+          observer.complete();
         }
-        return event;
-      }),
-      catchError((error: HttpErrorResponse) => {
-        this.loaderService.deactivateLoader();
-        this.snackBarsService.openSnackBarError('An error occurred. Please contact Admin.');
-        return throwError(error);
-      }));
+      );
+      // remove request from queue when cancelled
+      return () => {
+        this.removeRequest(request);
+        subscription.unsubscribe();
+      };
+    });
+  }
+
+  removeRequest(req: HttpRequest<any>) {
+    const i = this.requests.indexOf(req);
+    if (i >= 0) {
+      this.requests.splice(i, 1);
+    }
+    this.loaderService.isLoading.next(this.requests.length > 0);
   }
 }
