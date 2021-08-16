4bd60baf14adfee6bc4308c8891d53f0d345dd0a
Philip Shishov <philshishov@gmail.com>
2021-08-16 22:53:19 +0300

    complete fund module

--- src/client/src/app/modules/fund/fund-routing.module.ts
+++ src/client/src/app/modules/fund/fund-routing.module.ts
@@ -1,8 +1,19 @@
+import { FundsComponent } from './components/funds/funds.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from 'src/app/core/guards/auth-guard.service';
+import { AppLayoutComponent } from 'src/app/core/layouts/app-layout/components/container/app-layout.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    component: AppLayoutComponent,
+    canActivate: [AuthGuardService],
+    children: [
+      { path: '', component: FundsComponent }
+    ]
+  }
+]
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
--- src/client/src/app/modules/fund/fund.module.ts
+++ src/client/src/app/modules/fund/fund.module.ts
@@ -1,14 +1,25 @@
+import { TranslateModule } from '@ngx-translate/core';
+import { AppLayoutModule } from './../../core/layouts/app-layout/app-layout.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FundRoutingModule } from './fund-routing.module';
+import { FundsComponent } from './components/funds/funds.component';
+import { AllEntitesModule } from 'src/app/shared/allentities/allentities.module';
+import { FundsService } from './fund-service';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FundsComponent
+  ],
   imports: [
     CommonModule,
-    FundRoutingModule
-  ]
+    FundRoutingModule,
+    AppLayoutModule,
+    TranslateModule,
+    AllEntitesModule
+  ],
+  providers: [FundsService]
 })
 export class FundModule { }
