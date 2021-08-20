d72dab4467bd52d98a7d77b716891ec53035f650
Philip Shishov <philshishov@gmail.com>
2021-08-20 08:47:47 +0300

    loader component when waiting for page to load

---
+++ src/client/src/app/components/loader/loader.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { LoaderService } from 'src/app/shared/utils/loader.service';
+
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.scss']
+})
+export class LoaderComponent implements OnInit {
+
+  loading: boolean;
+
+  constructor(private loaderService: LoaderService) {
+
+    this.loaderService.isLoading.subscribe((v) => {
+      console.log(v);
+      this.loading = v;
+    });
+
+  }
+  ngOnInit() {
+  }
+
+}
