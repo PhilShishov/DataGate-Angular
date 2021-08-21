1bd95d04f99f03bae5cbefe1f9dd78a2cea84530
Philip Shishov <philshishov@gmail.com>
2021-08-21 23:01:05 +0300

    routing for main object models

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
   {
@@ -18,6 +17,12 @@ const routes: Routes = [
   },
   { path: '', data: { title: 'Legal' }, loadChildren: () => import('./modules/legal/legal.module').then(m => m.LegalModule) },
   { path: 'userpanel', data: { title: 'User Panel' }, loadChildren: () => import('./modules/user-panel/user-panel.module').then(m => m.UserPanelModule) },
+  { path: 'funds', data: { title: 'Funds' }, loadChildren: () => import('./modules/fund/fund.module').then(m => m.FundModule) },
+  { path: 'subfunds', data: { title: 'Sub Funds' }, loadChildren: () => import('./modules/sub-fund/sub-fund.module').then(m => m.SubFundModule) },
+  { path: 'shareclasses', data: { title: 'Share Classes' }, loadChildren: () => import('./modules/share-class/share-class.module').then(m => m.ShareClassModule) },
+  { path: 'search-results', data: { title: 'Search Results' }, loadChildren: () => import('./modules/search-result/search-result.module').then(m => m.SearchResultModule) },
+  { path: 'sc', data: { title: 'Share Classes' }, loadChildren: () => import('./modules/share-class/share-class.module').then(m => m.ShareClassModule) },
+  { path: 'admin', data: { title: 'Admin' }, loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule) },
   { path: '**', redirectTo: '/', data: { title: '404' } }
 ];
 
