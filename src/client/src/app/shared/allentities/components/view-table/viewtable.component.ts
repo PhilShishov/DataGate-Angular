5021cab70e6ee90f0bf9c51673e11b6561527e69
Philip Shishov <philshishov@gmail.com>
2021-11-11 22:26:06 +0100

    breakpoint observer adding

--- src/client/src/app/shared/allentities/components/view-table/viewtable.component.ts
+++ src/client/src/app/shared/allentities/components/view-table/viewtable.component.ts
@@ -4,17 +4,17 @@ import { IEntitiesViewModel } from './../../../interfaces/IEntitiesViewModel';
 import { AfterViewInit, Component, Input, OnInit, ViewChild } from "@angular/core";
 import { DataGateConstants } from 'src/app/shared/utils/constants';
 import { StringSwapper } from '../../../utils/stringSwapper';
-import { BehaviorSubject } from 'rxjs';
-import { scan } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 import { MatSort } from '@angular/material/sort';
 import { MatPaginator } from '@angular/material/paginator';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'all-viewtable',
   templateUrl: './viewtable.component.html',
   styleUrls: ['./viewtable.component.scss']
 })
-export class ViewTableComponent implements OnInit,AfterViewInit {
+export class ViewTableComponent implements OnInit, AfterViewInit {
   @Input() model: IEntitiesViewModel;
   @Input() controller: string;
   @Input() columns: Array<string>;
@@ -23,22 +23,35 @@ export class ViewTableComponent implements OnInit,AfterViewInit {
   IndexEntityIdInTable = DataGateConstants.indexEntityIdInTable;
   entityAbr: string;
   routeDetails: string;
+  isLargeScreen: boolean = false;
+
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild('paginator') paginator: MatPaginator;
   @ViewChild('tableToExtract') tableToExtract;
 
-  constructor(private datePipe: DatePipe) { }
+  constructor(private datePipe: DatePipe, private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
     this.entityAbr = StringSwapper.byController(this.controller, DataGateConstants.FundAbbreviation, DataGateConstants.SubFundAbbreviation, DataGateConstants.ShareClassAbbreviation);
     this.routeDetails = StringSwapper.byController(this.controller, DataGateConstants.RouteDetails + DataGateConstants.FundArea, DataGateConstants.RouteDetails + DataGateConstants.DisplaySub + DataGateConstants.FundArea, DataGateConstants.RouteDetails + DataGateConstants.ShareClassArea);
+    this.breakpointObserver.observe([
+      '(min-width: 600px)'
+    ]).pipe(
+      tap(result => this.isLargeScreen = result.matches)
+    ).subscribe(result => {
+      if (result.matches) {
+        this.dataSource.paginator = this.paginator;
+      } else {
+        this.dataSource.paginator = null;
+      }
+    });
   }
 
   getRoute(id) {
-    let date = this.datePipe.transform(new Date(),"yyyy-MM-dd");
+    let date = this.datePipe.transform(new Date(), "yyyy-MM-dd");
     return "/" + this.entityAbr + "/" + id + "/" + date;
   }
-  
+
   ngAfterViewInit(): void {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
@@ -53,7 +66,7 @@ export class ViewTableComponent implements OnInit,AfterViewInit {
       return [10, 25, 50, this.dataSource.data.length];
     }
     else {
-     return [10, 25, 50];
+      return [10, 25, 50];
     }
   }
 }
